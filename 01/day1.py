afc8055202342239ec152933215780b9e81fa328
Tobi <tobiasvalentine95@gmail.com>
2025-12-01 13:17:06 -0600

    part 1 done

---
+++ 01/day1.py
@@ -0,0 +1,41 @@
+# dial starts at 0
+# L subtracts, R adds (R10 dial = 50 + 10; L10 dial = 50 - 10)
+# 0 -> 99; 99 -> 0
+# we actually need to be counting the times the dial points at 0
+
+def dialOverflow(dial):
+    i = dial
+    if dial in range(0, 99):
+        return dial
+    while i not in range(0, 99):
+        if i > 99:
+            i -= 100
+        elif i < 0:
+            i += 100
+        else:
+            break
+    return i
+
+
+def day1():
+    rotations = []
+    dial = 50
+    zeroes = 0
+    # with open('test_input.txt', 'r') as file:
+    with open('input.txt', 'r') as file:
+        rotations = [line.rstrip('\n') for line in file]
+    # print(rotations)
+    for r in rotations:
+        if r[0] == "R":
+            dial += int(r[1:])
+            dial = dialOverflow(dial)
+        elif r[0] == "L":
+            dial -= int(r[1:])
+            dial = dialOverflow(dial)
+        if dial == 0:
+            zeroes += 1
+        # print(dial)
+    return print(f"Final Dial: {dial}\nZeroes: {zeroes}")
+
+
+day1()
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # advent-of-code-2025
+
+my solutions for [advent of code](https://adventofcode.com/2025)
